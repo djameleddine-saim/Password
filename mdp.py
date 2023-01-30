22485e40cfd5daf95dbd5ba318dfedcdb652daca
djamel <djamel-eddine.saim@laplateforme.io>
2023-01-30 11:29:11 +0100

    Password

--- mdp.py
+++ mdp.py
@@ -1,29 +1,31 @@
-import json
 import hashlib
+import json
 
 # Lire les mots de passe existants à partir du fichier passwords.json
 try:
     with open('passwords.json', 'r') as f:
         passwords = json.load(f)
+
 except:
-    # Dictionnaire vide pour stocker les mots de pass
+    # dictionnaire vide pour stocker les mots de pass
     passwords = {}
 
+# Boucle infinie pour demander les mots de passe à l'utilisateur
 while True:
+
     # Demander à l'utilisateur de choisir un mot de passe
     password = input("Choisissez un mot de passe: ")
-
     # Vérifier si le mot de passe est suffisamment long (8 caractères ou plus)
-    if len(password) < 8:
+    if len(password) < 8:  # Vérifier si le mot de passe est suffisamment long (8 caractères ou plus)
         print("Le mot de passe doit contenir au moins 8 caractères.")
 
     # Verifier si le mot de passe contient au moins une lettre minuscule
     elif not any(c in "abcdefghijklmnopqrstuvwxyz" for c in password):
-        print("Le mot de passe doit contenir au moins une lettre minuscule.")
+        print("Le mot de passe doit contenir au moins une lettre minuscule. ")
 
     # Vérifier si le mot de passe contient au moins une lettre majuscule
     elif not any(c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for c in password):
-        print("Le mot de passe doit contenir au moins une lettre majuscule.")
+        print("Le mot de passe doit contenir au moins une lettre  majuscule.")
 
     # Vérifier si le mot de passe contient au moins un chiffre
     elif not any(c in "0123456789" for c in password):
@@ -37,21 +39,14 @@ while True:
         print("Mot de passe valide!")
         # Crypter le mot de passe en utilisant l'algorithme SHA-256
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
         # vérifier si le mot de passe existe déjà dans les valeurs du dictionnaire
         if password in passwords:
-            if hashed_password in passwords[password]:
-                print("Ce mot de passe existe déjà.")
-            elif hashed_password not in passwords[password]:
-
-                # Ajouter le mot de passe et le mot de passe crypté au dictionnaire
-                passwords[password] += [hashed_password]
-                print("mot de pass: ", password)
-                print("Mot de passe crypté: ", hashed_password)
-                print("Mot de passe enregistré!", end='\n')
-
+            print("Ce mot de passe existe déjà.")
         else:
+            # Ajouter le mot de passe et le mot de passe crypté au dictionnaire
             passwords[password] = [hashed_password]
+            print("mot de pass: ", password)
+            print("Mot de passe crypte: ", hashed_password)
         # Demander à l'utilisateur s'il souhaite entrer un autre mot de passe
         repeat = input("Voulez-vous entrer un autre mot de passe? (o/n)")
         if repeat == 'n':
@@ -60,5 +55,4 @@ while True:
 # Enregistrer les mots de passe dans un fichier JSON
 with open('passwords.json', 'w') as f:
     # l'option separators et l'option indent pour séparer les différents mots de passe enregistrés dans le fichier JSON
-    json.dump(passwords, f, separators=(',', ': '), indent=4)
-
+    json.dump(passwords, f, separators=(', ', ': '), indent=4)
